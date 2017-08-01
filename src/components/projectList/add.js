0b42a366fd4a8f4d7b408d483a89c680eee9a900
naice <370215230@qq.com>
2017-08-01 10:14:14 +0800

    upload-image

--- src/components/projectList/add.js
+++ src/components/projectList/add.js
@@ -4,7 +4,7 @@
  * @date 2017/7/24
  */
 import React, {Component} from 'react'
-import {Input, Upload, Icon, Modal} from 'antd';
+import {Input, Upload, Icon, Modal, Button, message} from 'antd';
 import { connect } from 'react-redux';
 import store from '../../redux/store.js';
 import {getArticle, removeArticle} from '../../action/article'
@@ -16,23 +16,38 @@ class AddProject extends Component {
   state = {
     previewVisible: false,
     previewImage: '',
-    fileList: [{
-      uid: -1,
-      name: 'xxx.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    }],
+    fileList: [],
+    uploadfinishUrl: '',
+    title: '',
+    describe: '',
+    href: ''
   };
   handleCancel = () => this.setState({ previewVisible: false })
 
   handlePreview = (file) => {
+    console.log(file)
     this.setState({
       previewImage: file.url || file.thumbUrl,
       previewVisible: true,
     });
   }
 
-  handleChange = ({ fileList }) => this.setState({ fileList })
+  handleChange = ({ fileList }) => {
+    if (fileList[0].response && fileList[0].response.imgUrl) {
+      this.state.uploadfinishUrl = fileList[0].response.imgUrl
+    }
+    this.setState({ fileList })
+  }
+  _handleIputChange (str, e) {
+    this.state[str] = e.target.value
+  }
+  _subBtn() {
+    if (this.state.title && this.state.describe && this.state.href) {
+      console.log(123)
+    } else {
+      message.info('所有选线不能为空');
+    }
+  }
 
   render () {
     const { previewVisible, previewImage, fileList } = this.state;
@@ -45,17 +60,18 @@ class AddProject extends Component {
     return (
       <section id="project">
         <p>项目标题：</p>
-        <Input placeholder="项目标题" />
+        <Input onChange={this._handleIputChange.bind(this, 'title')} placeholder="项目标题" />
         <p>项目描述：</p>
-        <Input placeholder="项目描述" />
+        <Input onChange={this._handleIputChange.bind(this, 'describe')} placeholder="项目描述" />
         <p>项目连接：</p>
-        <Input placeholder="项目连接" />
+        <Input onChange={this._handleIputChange.bind(this, 'href')} placeholder="项目连接" />
         <p>上传图片：</p>
         <div className="clearfix">
           <Upload
-            action="//jsonplaceholder.typicode.com/posts/"
+            action="http://localhost:3030/api/backstage/upload"
             listType="picture-card"
             fileList={fileList}
+            name="file"
             onPreview={this.handlePreview}
             onChange={this.handleChange}
           >
@@ -65,6 +81,7 @@ class AddProject extends Component {
             <img alt="example" style={{ width: '100%' }} src={previewImage} />
           </Modal>
         </div>
+        <Button onClick={this._subBtn.bind(this)} className="subBtn" type="primary" size="large">提交</Button>
       </section>
     )
   }
