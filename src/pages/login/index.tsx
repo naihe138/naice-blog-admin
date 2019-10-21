3c7ca9aad83426e06f4763cf39204f0a86edc6a3
naihe138 <370215230@qq.com>
2019-10-21 16:41:16 +0800

    login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,53 @@
 import React from 'react'
-
-export default () => {
-  return <h1>login page</h1>
+import { Form, Icon, Input, Button } from 'antd'
+import { FormComponentProps } from 'antd/lib/form/Form'
+import './login.scss'
+function LoginForm (props: FormComponentProps) {
+  const { getFieldDecorator } = props.form
+  const handleSubmit = (e: React.FormEvent) => {
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  const iconColor = { color: 'rgba(0,0,0,.25)' }
+  return (
+    <div id="login">
+      <Form onSubmit={ handleSubmit } className="login-form">
+        <Form.Item>
+          {
+            getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={iconColor} />}
+                placeholder="Username"
+              />
+            )
+          }
+        </Form.Item>
+        <Form.Item>
+          {
+            getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={iconColor} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )
+          }
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
+
+export default Form.create({ name: 'blog_login' })(LoginForm)
